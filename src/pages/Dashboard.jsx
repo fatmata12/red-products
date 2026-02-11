0260dc915c30173f7bb597b815eca7f9ffba467b
fatmata12 <fc5434653@gmail.com>
2026-02-11 12:23:34 +0100

    Modification App.jsx et routes accueil

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { FaBars } from "react-icons/fa";
+import { FaBars, FaFileAlt, FaEnvelope, FaComments, FaHotel, FaUsers, FaBuilding } from "react-icons/fa";
 import "./dashboard.css";
 
 export default function Dashboard() {
@@ -8,28 +8,62 @@ export default function Dashboard() {
   return (
     <div className="layout">
 
-      {/* Sidebar */}
+      {/* SIDEBAR */}
       <div className={`sidebar ${isOpen ? "open" : "closed"}`}>
         <h2 className="logo">PRODUIT ROUGE</h2>
         <ul>
           <li>Tableau de bord</li>
-          <li>Liste des hôtels</li>
+          <li>Liste des hotels</li>
         </ul>
       </div>
 
-      {/* Main */}
+      {/* MAIN */}
       <div className={`main ${isOpen ? "shifted" : ""}`}>
-        
-        {/* Hamburger */}
-        <button 
-          className="hamburger"
-          onClick={() => setIsOpen(!isOpen)}
-        >
+
+        {/* HAMBURGER */}
+        <button className="hamburger" onClick={() => setIsOpen(!isOpen)}>
           <FaBars />
         </button>
 
-        <h1>Dashboard</h1>
+        <div className="cards">
 
+          <div className="card purple">
+            <FaFileAlt />
+            <h3>FORMULAIRES</h3>
+            <p>125</p>
+          </div>
+
+          <div className="card green">
+            <FaComments />
+            <h3>MESSAGES</h3>
+            <p>40</p>
+          </div>
+
+          <div className="card red">
+            <FaEnvelope />
+            <h3>EMAILS</h3>
+            <p>25</p>
+          </div>
+
+          <div className="card violet">
+            <FaHotel />
+            <h3>HÔTELS</h3>
+            <p>40</p>
+          </div>
+
+          <div className="card orange">
+            <FaUsers />
+            <h3>UTILISATEURS</h3>
+            <p>600</p>
+          </div>
+
+          <div className="card blue">
+            <FaBuilding />
+            <h3>ENTITÉS</h3>
+            <p>02</p>
+          </div>
+
+        </div>
       </div>
     </div>
   );
